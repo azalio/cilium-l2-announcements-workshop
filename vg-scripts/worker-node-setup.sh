dae42c4a6b2cf187d0973bab8bc5c280a540e3ff
Mikhail [azalio] Petrov <azalio@azalio.net>
2025-01-18 16:30:44 +0300

    chore: Comment out apt-get and Kubernetes config copy commands

--- vg-scripts/worker-node-setup.sh
+++ vg-scripts/worker-node-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Ensure packages are up to date
-apt-get update
-apt-get upgrade -y
+# apt-get update
+# apt-get upgrade -y
 
 # Получение команды для присоединения к кластеру
 JOIN_COMMAND=$(ssh -o StrictHostKeyChecking=no root@server "kubeadm token create --print-join-command")
@@ -11,6 +11,6 @@ JOIN_COMMAND=$(ssh -o StrictHostKeyChecking=no root@server "kubeadm token create
 $JOIN_COMMAND
 
 # Копирование конфигурации Kubernetes на сервер
-log "Copying Kubernetes config to server..."
-scp /root/.kube/config root@server:/root/.kube/config_node_$(hostname)
-ssh root@server "chmod 600 /root/.kube/config_node_$(hostname)"
+# log "Copying Kubernetes config to server..."
+# scp /root/.kube/config root@server:/root/.kube/config_node_$(hostname)
+# ssh root@server "chmod 600 /root/.kube/config_node_$(hostname)"
