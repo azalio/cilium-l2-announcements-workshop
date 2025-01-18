426d54e1185068add56feebe7672e855faa0829c
Mikhail [azalio] Petrov <azalio@azalio.net>
2025-01-18 18:58:29 +0300

    fix: specify kubeconfig in podCIDR patch command

--- vg-scripts/control-plane-setup.sh
+++ vg-scripts/control-plane-setup.sh
@@ -107,6 +107,6 @@ rm get_helm.sh
 log "Helm installed successfully"
 
 # Configure podCIDR for the server node
-kubectl patch node server --type merge -p '{"spec":{"podCIDR":"10.200.0.0/24"}}'
+kubectl --kubeconfig=/etc/kubernetes/kubelet.conf patch node server --type merge -p '{"spec":{"podCIDR":"10.200.0.0/24"}}'
 
 log "Configured podCIDR for server node"
